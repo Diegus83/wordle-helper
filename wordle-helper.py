4bcf0ff1af4a5148618eff6b8129136e03761467
Diego Alifano <diegus83@gmail.com>
2022-05-19 12:31:25 -0400

    Options menu reworked

--- wordle-helper.py
+++ wordle-helper.py
@@ -9,38 +9,73 @@ def loadWordList() -> list:
 	return words
 
 
-def filterBadLetters(letters: list):
-	"Eliminates words based on bad letter guesses"
-
+def graySquares(letters: list):
+	"Filter words using a bad letter (gray squares)"
 	return None
 
-def filterKnownPositions(letter: str, position: int):
-	"Eliminates words based on known letters and positions"
-
+def yellowSquares(letter: str, position: int):
+	"Filter words missing a known letter (yellow squares)"
 	return None
 
-def filterGoodLetters(letters: list):
-	"Eliminates words that don't contain a known good letter"
-
+def greenSquares(letters: list):
+	"Filter words missing a known letter and position (green squares)"
 	return None
 
-def printWords():
-	global word_list
-	print(word_list)
+def printWords(flag):
+	if not flag:
+		global word_list
+		for word in word_list:
+			print(word, end='\t')
+		print('')
+	else:
+		print(f'The current list contains {len(word_list)} words')
+
+def printMenu():
+	global menuOptions
+	for key in menuOptions.keys():
+		print(f'{key} - {menuOptions[key]}')
+
+def quitConfirmation():
+	print("Ok byeee!")
+	exit()
+
 
 word_list = loadWordList()
 
-while True:
-	print(f'The current list contains {len(word_list)} possible words')
-	print('- Choose an option to elimitate words from the list')
-	print('- Eliminate words that contain a bad letter (1)')
-	print('- Elminate words that are missing a good letter (2)')
-	print('- Eliminate words that don\'t have a letter in a known position (3)')
-	print('- Print list (p)')
-	print('- Quit (q)')
-	selection = input("Choose an option: ")
-	if selection == 'q':
-		print('Ok, bye!' )
-		break
-	elif selection == 'p':
-		printWords()
+menuOptions = {
+	1: "Print current list of words",
+	2: "Filter words using a bad letter (gray squares)",
+	3: "Filter words missing a known letter (yellow squares)",
+	4: "Filter words missing a known letter and position (green squares)",
+	5: "Quit",
+	6: "Start over"
+}
+
+menuKeys = [x for x in menuOptions.keys()]
+
+if __name__ == "__main__":
+	while True:
+		printWords(True)
+		printMenu()
+		
+		try:
+			option = int(input("Select: "))
+		except:
+			option = 9999
+
+		if option == 1:
+			printWords(False)
+		elif option == 2:
+			graySquares()
+		elif option== 3:
+			yellowSquares()
+		elif option == 4:
+			greenSquares()
+		elif option == 5:
+			quitConfirmation()
+		elif option == 6:
+			loadWordList()
+
+		else:
+			print(f'Invalid option, please choose a number between {menuKeys[0]} and {menuKeys[-1]}')
+
